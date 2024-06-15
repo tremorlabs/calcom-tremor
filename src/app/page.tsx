094719a960690781b04b14bfd41347d10ef94a20
christopherkindl <53372002+christopherkindl@users.noreply.github.com>
2024-06-15 21:18:43 +0200

    add dummy chart

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,63 @@
-import Image from "next/image";
+"use client"
+
+import { BarChart } from "@/components/BarChart"
+
+const data =
+  [
+    { month: "June, 2021", "Distinct values of userId": 7 },
+    { month: "July, 2021", "Distinct values of userId": 29 },
+    { month: "August, 2021", "Distinct values of userId": 33 },
+    { month: "September, 2021", "Distinct values of userId": 322 },
+    { month: "October, 2021", "Distinct values of userId": 489 },
+    { month: "November, 2021", "Distinct values of userId": 496 },
+    { month: "December, 2021", "Distinct values of userId": 733 },
+    { month: "January, 2022", "Distinct values of userId": 1099 },
+    { month: "February, 2022", "Distinct values of userId": 1241 },
+    { month: "March, 2022", "Distinct values of userId": 1288 },
+    { month: "April, 2022", "Distinct values of userId": 1427 },
+    { month: "May, 2022", "Distinct values of userId": 1463 },
+    { month: "June, 2022", "Distinct values of userId": 1304 },
+    { month: "July, 2022", "Distinct values of userId": 1460 },
+    { month: "August, 2022", "Distinct values of userId": 1531 },
+    { month: "September, 2022", "Distinct values of userId": 2423 },
+    { month: "October, 2022", "Distinct values of userId": 3065 },
+    { month: "November, 2022", "Distinct values of userId": 3542 },
+    { month: "December, 2022", "Distinct values of userId": 3481 },
+    { month: "January, 2023", "Distinct values of userId": 5269 },
+    { month: "February, 2023", "Distinct values of userId": 5734 },
+    { month: "March, 2023", "Distinct values of userId": 6882 },
+    { month: "April, 2023", "Distinct values of userId": 6877 },
+    { month: "May, 2023", "Distinct values of userId": 8061 },
+    { month: "June, 2023", "Distinct values of userId": 8241 },
+    { month: "July, 2023", "Distinct values of userId": 8880 },
+    { month: "August, 2023", "Distinct values of userId": 10062 },
+    { month: "September, 2023", "Distinct values of userId": 11125 },
+    { month: "October, 2023", "Distinct values of userId": 12761 },
+    { month: "November, 2023", "Distinct values of userId": 13536 },
+    { month: "December, 2023", "Distinct values of userId": 13039 },
+    { month: "January, 2024", "Distinct values of userId": 17057 },
+    { month: "February, 2024", "Distinct values of userId": 18773 },
+    { month: "March, 2024", "Distinct values of userId": 20549 },
+    { month: "April, 2024", "Distinct values of userId": 22767 },
+    { month: "May, 2024", "Distinct values of userId": 24649 }
+  ]
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main className="p-4 sm:p-10">
+      <BarChart
+        className="h-[450px]"
+        data={data}
+        index="month"
+        categories={["Distinct values of userId"]}
+        valueFormatter={(number: number) =>
+          `${Intl.NumberFormat("us").format(number).toString()}`
+        }
+        onValueChange={(v) => console.log(v)}
+        xAxisLabel="Month"
+        yAxisLabel="Active users"
+      />
     </main>
   );
 }
