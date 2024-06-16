09e86c83bdcaf187db2426b1488dba68e8568897
christopherkindl <53372002+christopherkindl@users.noreply.github.com>
2024-06-16 16:31:10 +0200

    refactor: links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,32 +2,32 @@
 import Charts from "../components/charts";
 
 type RawMonthlyActiveUser = {
-    month: string
-    "Distinct values of userId": string
+  month: string
+  "Distinct values of userId": string
 }
 
 export type MonthlyActiveUser = {
-    month: string
-    "Monthly active users": number
+  month: string
+  "Monthly active users": number
 }
 
 async function getData(): Promise<MonthlyActiveUser[]> {
-    const res = await fetch('https://t.co/Uq262K4jai')
-    if (!res.ok) {
-        throw new Error('Failed to fetch data')
-    }
+  const res = await fetch('https://cal.metabaseapp.com/public/question/81ced336-2644-47f3-ae2f-6bda42f2399d.json')
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
 
-    const data: RawMonthlyActiveUser[] = await res.json()
+  const data: RawMonthlyActiveUser[] = await res.json()
 
-    return data.map(item => ({ ...item, "Monthly active users": parseInt(item['Distinct values of userId'].replace(/,/g, ''), 10) }))
+  return data.map(item => ({ ...item, "Monthly active users": parseInt(item['Distinct values of userId'].replace(/,/g, ''), 10) }))
 }
 
 export default async function Home() {
-    const data = await getData()
+  const data = await getData()
 
-    return (
-        <main className="p-4 sm:p-10 space-y-6">
-            <Charts data={data} />
-        </main>
-    );
+  return (
+    <main className="p-4 sm:p-10">
+      <Charts data={data} />
+    </main>
+  );
 }
--- src/components/charts.tsx
+++ src/components/charts.tsx
@@ -1,15 +1,13 @@
 "use client"
 
 import { BarChart } from "@/components/BarChart"
-import { AreaChart } from "@/components/AreaChart";
 import { MonthlyActiveUser } from "../app/page";
 
 type ChartsProps = {
-    data: MonthlyActiveUser[]
+  data: MonthlyActiveUser[]
 }
 
-export default async function Charts({data}: ChartsProps){
-
+export default function Charts({ data }: ChartsProps) {
   return (
     <>
       <BarChart
@@ -20,19 +18,7 @@ export default async function Charts({data}: ChartsProps){
         valueFormatter={(number: number) =>
           `${Intl.NumberFormat("us").format(number).toString()}`
         }
-        onValueChange={(v) => console.log(v)}
-        xAxisLabel="Month"
-        yAxisLabel="Active users"
-      />
-      <AreaChart
-        className="h-[450px]"
-        data={data}
-        index="month"
-        categories={["Monthly active users"]}
-        valueFormatter={(number: number) =>
-          `${Intl.NumberFormat("us").format(number).toString()}`
-        }
-        onValueChange={(v) => console.log(v)}
+        // onValueChange={(v) => console.log(v)}
         xAxisLabel="Month"
         yAxisLabel="Active users"
       />
